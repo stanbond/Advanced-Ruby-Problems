038e461ad4417601d8a486a69e4483d9585f06b1
StanBond <stan.bond@ymail.com>
2020-01-09 19:00:05 -0800

    last index

--- Advanced-Ruby/AdvancedProblems.rb
+++ Advanced-Ruby/AdvancedProblems.rb
@@ -24,4 +24,17 @@ end
 print o_words("How did you do that?") #=> ["How", "you", "do"]
 puts
 
-#
+# Write a method last_index that takes in a string and a character. The method should return the last index where the character can be found in the string.
+
+def last_index(str, char)
+	i = str.length - 1
+  while i >= 0
+    return i if char == str[i]
+    i -= 1
+  end
+end
+
+puts last_index("abca", "a")       #=> 3
+puts last_index("mississipi", "i") #=> 9
+puts last_index("octagon", "o")    #=> 5
+puts last_index("programming", "m")#=> 7
