b1b92402a2a28f2afdbdac072c69d4a006b3a4db
StanBond <stan.bond@ymail.com>
2020-01-09 18:50:38 -0800

    select

--- Advanced-Ruby/AdvancedProblems.rb
+++ Advanced-Ruby/AdvancedProblems.rb
@@ -1,4 +1,4 @@
-#1 Write a method yell_sentence that takes in a sentence string and returns a new sentence where every word is yelled. Use map to solve this.
+# Write a method yell_sentence that takes in a sentence string and returns a new sentence where every word is yelled. Use map to solve this.
 
 def yell_sentence(sent)
   sent.upcase.split.map { |word| word + '! ' }.join
@@ -6,7 +6,7 @@ end
 
 puts yell_sentence("I have a bad feeling about this") #=> "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
 
-#2 Write a method whisper_words that takes in an array of words and returns a new array containing a whispered version of each word. Solve this using map.
+# Write a method whisper_words that takes in an array of words and returns a new array containing a whispered version of each word. Solve this using map.
 
 def whisper_words(words)
   words.map { |arr| arr.downcase + "... " }
@@ -14,3 +14,14 @@ end
 
 print whisper_words(["KEEP", "The", "NOISE", "down"]) # => ["keep...", "the...", "noise...", "down..."]
 puts
+
+# Write a method o_words that takes in a sentence string and returns an array of the words that contain an "o". Use select in your solution!
+
+def o_words(sentence)
+	sentence.split.select { |word| word.include?("o")}
+end
+
+print o_words("How did you do that?") #=> ["How", "you", "do"]
+puts
+
+#
