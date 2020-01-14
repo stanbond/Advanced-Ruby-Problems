6b75e9889c5cd1198d41879a80ff79fc9c96c316
StanBond <stan.bond@ymail.com>
2020-01-13 16:02:18 -0800

    hash

--- Advanced-Ruby/AdvancedProblems.rb
+++ Advanced-Ruby/AdvancedProblems.rb
@@ -56,3 +56,20 @@ def count(words)
 end
 
 print most_vowels("what a wonderful life") #=> "wonderful"
+
+# Write a method prime? that takes in a number and returns a boolean, indicating whether the number is prime. A prime number is only divisible by 1 and itself.
+
+def prime?(num)
+return false if num < 2
+  (2...num).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
+
+puts prime?(2)  #=> true
+puts prime?(5)  #=> true
+puts prime?(11) #=> true
+puts prime?(4)  #=> false
+puts prime?(9)  #=> false
+puts prime?(25) #=> false
