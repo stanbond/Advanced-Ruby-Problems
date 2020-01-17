09e393e264497a0b19e96015dc4f3a451daef272
StanBond <stan.bond@ymail.com>
2020-01-16 17:31:01 -0800

    primes

--- Advanced-Ruby/AdvancedProblems.rb
+++ Advanced-Ruby/AdvancedProblems.rb
@@ -73,3 +73,21 @@ puts prime?(11) #=> true
 puts prime?(4)  #=> false
 puts prime?(9)  #=> false
 puts prime?(25) #=> false
+
+# Write a method pick_primes that takes in an array of numbers and returns a new array containing only the prime numbers.
+
+def pick_primes(numbers)
+	numbers.select { |num| prime?(num)}
+end
+def prime?(num)
+  return false if num < 2
+    (2...num).each do |n|
+      return false if num % n == 0
+    end
+  true
+end
+  
+print pick_primes([2, 3, 4, 5, 6]) #=> [2, 3, 5]
+puts
+print pick_primes([101, 20, 103, 2017]) #=> [101, 103, 2017]
+puts
